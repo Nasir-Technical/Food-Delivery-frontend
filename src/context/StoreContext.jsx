05b21c2cca9ae9b3bde33b1c00ff8799b67c19c3
Nasir-Technical <nasir.18.2006@gmail.com>
2024-08-13 01:23:35 +0500

    changes

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = 'https://food-delivery-backend-eta.vercel.app';
+    const url = 'https://food-delivery-backend-lake.vercel.app';
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
 
