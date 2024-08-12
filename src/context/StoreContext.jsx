55f197248e7e76fd7c5949b383b893f153e1ac04
Nasir-Technical <nasir.18.2006@gmail.com>
2024-08-12 21:14:09 +0500

    changes

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -8,8 +8,8 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
     const url = "http://localhost:4000";
-    const [token,setToken] = useState("");
-    const [food_list,setFoodList] = useState([]);
+    const [token, setToken] = useState("");
+    const [food_list, setFoodList] = useState([]);
 
     const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
@@ -19,14 +19,14 @@ const StoreContextProvider = (props) => {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
         if (token) {
-            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+            await axios.post(url + "/api/cart/add", { itemId }, { headers: { token } })
         }
     }
 
     const removeFromCart = async (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
         if (token) {
-            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+            await axios.post(url + "/api/cart/remove", { itemId }, { headers: { token } })
         }
     }
 
@@ -42,18 +42,29 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
-    const fetchFoodList =  async () => {
-        const response = await axios.get(url+"/api/food/list");
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list");
         setFoodList(response.data.data)
     }
 
     const loadCartData = async (token) => {
-        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
-        setCartItems(response.data.cartData);
+
+        try {
+            const response = await axios.post(url + "/api/cart/get", {}, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            setCartItems(response.data.cartData);
+            console.log("Success hogaya!");
+        } catch (error) {
+            console.log("Error agaya!");
+            console.log(error);
+        }
     }
 
-    useEffect(()=>{
-      
+    useEffect(() => {
+
         async function loadData() {
             await fetchFoodList();
             if (localStorage.getItem("token")) {
@@ -62,7 +73,7 @@ const StoreContextProvider = (props) => {
             }
         }
         loadData();
-    },[])
+    }, [])
 
     const contextValue = {
         food_list,
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import StoreContextProvider from './context/StoreContext.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import StoreContextProvider from './context/StoreContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
-  </BrowserRouter>
-  
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
