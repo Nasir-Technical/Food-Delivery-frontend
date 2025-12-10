8a5ea815ecad42cb9d25ac8bf99912f2ac48d57e
Nasir-Technical <nasir.18.2006@gmail.com>
2025-12-10 19:53:11 +0500

    changes in storeContext

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
   const [food_list, setFoodList] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const url = "https://food-delivery-backend-nu-seven.vercel.app"; // ✅ No trailing slash
+  const url = "https://food-delivery-backend-nu-seven.vercel.app";
   // const url = "http://localhost:4000"; // ✅ No trailing slash
 
   const addToCart = async (itemId) => {
